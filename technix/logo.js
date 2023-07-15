80c15c25291d630a9b0365dd758d019dd714988a
NavneetKaurBhatti <navneetkaur81960@gmail.com>
2023-07-15 01:43:49 -0400

    make logo page common

---
+++ technix/logo.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var logoContainer = document.getElementById('logoPlaceholder');
+  logoContainer.innerHTML = `
+    <div class="tp-header-logo ms-xxl-5 ms-xl-5">
+      <a href="index.html">
+        <h2>Drora</h2>
+      </a>
+    </div>
+  `;
+});
